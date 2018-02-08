ec358079a55596a8a33357e9ba021df521e97597
hadoop <527622077@qq.com>
2018-02-08 16:48:09 +0800

    initial version

--- src/test/java/SparkTest.scala
+++ src/test/java/SparkTest.scala
@@ -3,7 +3,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SparkTest {
 
   def main (args : Array[String]): Unit ={
-    val conf = new SparkConf().setAppName("Spark Test2").setMaster("spark://172.19.30.17:7077")
+    val conf = new SparkConf().setAppName("Spark Test2").setMaster("spark://172.19.30.17:7077").setJars(List("/home/hadoop/software/idea-IU-173.4548.28/SparkP7/out/artifacts/SparkP7_jar/SparkP7.jar"))
 
     val sc = new SparkContext(conf)
 
